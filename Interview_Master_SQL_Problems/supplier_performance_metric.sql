555aa40a31e547e933d821cb4ff57054a312c579
Vipul Khandelwal <87986956+VipulKhandelwal04@users.noreply.github.com>
2025-08-07 17:48:16 +0000

    new file:   Interview_Master_SQL_Problems/supplier_performance_metric.sql

---
+++ Interview_Master_SQL_Problems/supplier_performance_metric.sql
@@ -0,0 +1,13 @@
+-- Situation:
+-- As a Data Analyst on the Supply Chain Procurement Team, 
+-- you are tasked with assessing supplier performance to ensure
+-- reliable delivery of critical components. Your goal is to identify 
+-- the most active suppliers, understand which suppliers dominate in 
+-- specific manufacturing regions, and pinpoint any gaps in supply to 
+-- the Asia region. By leveraging data, you will help optimize vendor selection 
+-- strategies and mitigate potential supply chain risks.
+
+-- Queation 1:
+-- We need to know who our most active suppliers are. Identify the top 5 
+-- suppliers based on the total volume of components delivered in October 2024.
+
